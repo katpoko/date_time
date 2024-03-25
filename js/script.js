4cbf659c3b17ee343290fa674ddfb1aea4b4ebbf
Kat Pokorny <145707652+katpoko@users.noreply.github.com>
2024-03-25 03:01:39 +0000

    Update styles and add gradient animation

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@
 const lightDark = document.querySelector('a');
 const body = document.querySelector('body');
 
+const gradientElement = document.getElementById("gradient");
+let currentColorIndex = 0;
+const colors = ["red", "orange", "yellow", "blue", "blueviolet"];
+
+
 
 // Manually add dark class
 body.classList.add('dark');
@@ -14,9 +19,19 @@ body.classList.add('dark');
 
 lightDark.addEventListener('click', ()=> {
 
+
+  function changeGradient() {
+    currentColorIndex = (currentColorIndex + 1) % colors.length;
+    const nextColor = colors[currentColorIndex];
+    gradientElement.style.backgroundImage = `linear-gradient(to right, ${colors[currentColorIndex - 1]}, ${nextColor})`;
+  }
 //   add 'dark' class
 body.classList.toggle('dark');
   
+
+setInterval(changeGradient, 5000); // Change gradient every 5 seconds
+
+changeGradient(); // Trigger initial change
   //   check check
  console.log("ok?");
 });
